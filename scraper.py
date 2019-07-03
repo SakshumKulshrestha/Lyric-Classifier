2c95846a72cc7afbf43ea884b95b79d7d1333316
SakshumKulshrestha <sakshum2001@gmail.com>
2019-07-03 15:06:34 -0400

    built AZLyrics scraper

---
+++ scraper.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+
+class scraper: 
+
+    
+    def stripString(str1):
+        a = "!@#$%^&*()><:;[]|\?/~`'. "
+        for b in a:
+            str1 = str1.replace(b, '')
+
+        return str1.lower()
+
+    def getLyrics(song, name):
+        url = "https://www.azlyrics.com/lyrics/{}/{}.html".format(name.lower(), song.lower())
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'lxml')
+        divset = soup.find_all("div", limit=22)
+        lyrics = divset[len(divset)-1].text
+        return lyrics
+
+    def getSongs(name):
+        name = str(name)
+        url = "https://www.azlyrics.com/{}/{}.html".format(name[0], name)
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'lxml')
+        songsList = []
+
+        for title in soup.find_all(target="_blank"):
+            songsList.append(stripString(str(title.text)))
+
+        return songsList
+
+
+
+scrap = scraper
+artistName = 'eminem'
+songs = scrap.getSongs(artistName)
+dict = {}
+print(songs[3])
+song = scrap.stripString(songs[3])
+print(scrap.getLyrics(song, artistName))
+
+
+
+
+
+        
+

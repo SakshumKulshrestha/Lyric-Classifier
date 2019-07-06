2069801cea851b5fc87062be10b45280272bc9af
SakshumKulshrestha <sakshum2001@gmail.com>
2019-07-06 15:32:38 -0400

    Finish cleaning up data

--- transformData.py
+++ transformData.py
@@ -1,5 +1,5 @@
 def stripString(str1):
-        a = "!@#$%^&*()><:;[]|\?/~`'."
+        a = "!@#$%^&*()><:;,[]|\?/~`'."
         for b in a:
             str1 = str1.replace(b, '')
 
@@ -9,25 +9,51 @@ def printLyr(filepath):
     file = open(filepath, "r")
     lyrics = []
 
-    while file.readline():
+    for cnt, line in enumerate(file):
         song = ""
         while file.readline() != "@@@@@@@@@@\n":
             song += " " + stripString(file.readline())
         lyrics.append(song)
+        file.readline()
+
+    file.close()
+    return lyrics
+
+def printLyr_d(filepath):
+    file = open(filepath, "r")
+    lyrics = []
+
+    for cnt, line in enumerate(file):
+        song = ""
+        while file.readline() != "@@@@@@@@@@\n":
+            song += " " + stripString(file.readline())
+        lyrics.append(song)
+        file.readline()
 
     file.close()
     return lyrics
 
 
-filepath_eminem = "C:\Sakshum\MY STUFF\Random\Code Bits\eminem-lyrics-short.txt"
-filepath_drake = "C:\Sakshum\MY STUFF\Random\Code Bits\drake-lyrics.txt"
-
+filepath_eminem = "C:\\Users\Sakshum\Documents\GitHub\Lyric Project\Lyric-Classifier\eminem-lyrics"
 eminem_list = printLyr(filepath_eminem)
-drake_list = printLyr(filepath_drake)
+
+filepath_drake = "C:\\Users\Sakshum\Documents\GitHub\Lyric Project\Lyric-Classifier\drake-lyrics2"
+drake_list = printLyr_d(filepath_drake)
 
 file = open("songsFormatted", "w+")
+file_rappers = open("rappers", "w+")
 for song in eminem_list:
-    file.write(song + "\n"*2)
+    file.write(song + "\n")
+    file_rappers.write(str(0) + "\n")
+
+for song in drake_list:
+    file.write(song + "\n")
+    file_rappers.write(str(1) + "\n")
+
+file.close()
+file_rappers.close()
+
+
 
 
 
--- writeLyricstoFile.py
+++ writeLyricstoFile.py
@@ -1,17 +1,20 @@
 from scraper import scraper as scrap
+import time
 
 em_lyr = open("eminem-lyrics", "w")
-drake_lyr = open("drake-lyrics", "w")
+drake_lyr = open("drake-lyrics2", "w")
 
 eminem_songs = scrap.getSongs('eminem')
 drake_songs = scrap.getSongs('drake') 
 
 for song in eminem_songs:
     em_lyr.write(scrap.getLyrics(song, 'eminem'))
+    time.sleep(2)
     em_lyr.write("@"*10 + "\n")
 
 for song in drake_songs:
     drake_lyr.write(scrap.getLyrics(song, 'drake'))
+    time.sleep(3)
     drake_lyr.write("@"*10 + "\n")
 
 em_lyr.close()

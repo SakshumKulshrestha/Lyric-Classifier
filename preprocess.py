add3ca985431d064fccf2a9e15c03c9e6657592c
SakshumKulshrestha <sakshum2001@gmail.com>
2019-07-08 21:57:36 -0400

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,31 +1,30 @@
+
+import _pickle as pickle
+import numpy
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectPercentile, f_classif
+
 class preprocess:
-
-    import pickle
-    import _pickle as cPickle
-    import numpy
-
-    from sklearn.model_selection import train_test_split
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    from sklearn.feature_selection import SelectPercentile, f_classif
-
 
 
     def vectorize(words_file = "rapper_pickle.pkl", authors_file="songs_pickle.pkl"):
 
         authors_file_handler = open(authors_file, "rb")
-        authors = cPickle.load(authors_file_handler)
+        authors = pickle.load(authors_file_handler)
         authors_file_handler.close()
 
         words_file_handler = open(words_file, "rb")
-        word_data = cPickle.load(words_file_handler)
+        word_data = pickle.load(words_file_handler)
         words_file_handler.close()
 
 
         features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+        print(features_train + "\n" + "@"*10)
 
 
-
-        vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
+        vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words=None)
         features_train_transformed = vectorizer.fit_transform(features_train)
         features_test_transformed  = vectorizer.transform(features_test)
 

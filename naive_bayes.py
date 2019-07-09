e8694bdbc623a2c7dc385a3d4a877d83e5304682
SakshumKulshrestha <sakshum2001@gmail.com>
2019-07-08 22:08:53 -0400

    finished editing Naive Bayes implementation

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,7 +1,7 @@
 from preprocess import preprocess as p
-from sklearn.naive_bayes import GaussianNB as classifier
+from sklearn.naive_bayes import GaussianNB
 
-
+classifier = GaussianNB()
 f_train, f_test, l_train, l_test = p.vectorize()
 classifier.fit(f_train, l_train)
 

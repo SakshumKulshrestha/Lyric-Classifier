5259a0e5da2fb24d6d82b97a4292cd6bdda0fcb6
SakshumKulshrestha <sakshum2001@gmail.com>
2019-07-08 21:44:21 -0400

    editing preprocess

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,7 +1,8 @@
-from preprocess import preprocess
+from preprocess import preprocess as p
 from sklearn.naive_bayes import GaussianNB as classifier
 
-f_train, f_test, l_train, l_test = preprocess()
+
+f_train, f_test, l_train, l_test = p.vectorize()
 classifier.fit(f_train, l_train)
 
 print("accuracy: " + str(classifier.score(f_test, l_test)))
--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,38 @@
-import pickle
-import _pickle as cPickle
-import numpy
+class preprocess:
 
-from sklearn import cross_validation
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_selection import SelectPercentile, f_classif
+    import pickle
+    import _pickle as cPickle
+    import numpy
 
-
-
-def preprocess(words_file = "rapper_pickle.pkl", authors_file="songs_pickle.pkl"):
-
-    authors_file_handler = open(authors_file, "rb")
-    authors = pickle.load(authors_file_handler)
-    authors_file_handler.close()
-
-    words_file_handler = open(words_file, "rb")
-    word_data = cPickle.load(words_file_handler)
-    words_file_handler.close()
-
-
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    from sklearn.model_selection import train_test_split
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.feature_selection import SelectPercentile, f_classif
 
 
 
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
-    features_train_transformed = vectorizer.fit_transform(features_train)
-    features_test_transformed  = vectorizer.transform(features_test)
+    def vectorize(words_file = "rapper_pickle.pkl", authors_file="songs_pickle.pkl"):
+
+        authors_file_handler = open(authors_file, "rb")
+        authors = cPickle.load(authors_file_handler)
+        authors_file_handler.close()
+
+        words_file_handler = open(words_file, "rb")
+        word_data = cPickle.load(words_file_handler)
+        words_file_handler.close()
 
 
-    selector = SelectPercentile(f_classif, percentile=10)
-    selector.fit(features_train_transformed, labels_train)
-    features_train_transformed = selector.transform(features_train_transformed).toarray()
-    features_test_transformed  = selector.transform(features_test_transformed).toarray()
+        features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
-    return features_train_transformed, features_test_transformed, labels_train, labels_test
+
+
+        vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
+        features_train_transformed = vectorizer.fit_transform(features_train)
+        features_test_transformed  = vectorizer.transform(features_test)
+
+
+        selector = SelectPercentile(f_classif, percentile=10)
+        selector.fit(features_train_transformed, labels_train)
+        features_train_transformed = selector.transform(features_train_transformed).toarray()
+        features_test_transformed  = selector.transform(features_test_transformed).toarray()
+
+        return features_train_transformed, features_test_transformed, labels_train, labels_test
